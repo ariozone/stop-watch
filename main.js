6a25d6544285454877b3da6590ffd66578c57298
Ario Tamaddoni <ariozone6@gmail.com>
2018-08-30 14:00:41 -0700

    Add an event listener for clicks on the start button. #1

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var button = document.getElementById('func')
 var time = document.getElementById('timer')
-//var counter = parseInt(time.textContent, 10)
-func.addEventListener('click', function () {
-  var intervalId = setInterval(functon () {
-    time.textContent += 1
-    //time.textContent = counter
-   }, 1000)
+var counter = parseInt(time.textContent, 10)
+
+button.addEventListener('click', function(){
+  console.log('clicked')
+})
