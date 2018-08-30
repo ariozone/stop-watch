e8770191a8b62d4486c766e72f6f3c2524b72697
Ario Tamaddoni <ariozone6@gmail.com>
2018-08-30 16:23:28 -0700

    works saved

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 var button = document.getElementById('func')
 var time = document.getElementById('timer')
 var counter = parseInt(time.textContent, 10)
-
+var intervalId
 button.addEventListener('click', function(){
-  var intervalId = setInterval(update, 1000)
-})
-function update() {
-  counter += 1
-  time.textContent = counter
+     intervalId = setInterval(update, 1000)
 
-}
+})
+  function update() {
+      counter += 1
+      time.textContent = counter
+  }
