0b784daf90b807692158f8be83d15f9ef4ae2fc0
Ario Tamaddoni <ariozone6@gmail.com>
2018-08-30 18:23:05 -0700

    Interval cleared when start button is clicked for the second time. #2

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var time = document.getElementById('timer')
 var counter = parseInt(time.textContent, 10)
 var intervalId
 button.addEventListener('click', function(){
-     intervalId = setInterval(update, 1000)
-
-})
+ if (counter === 0) {
+  intervalId = setInterval(update, 1000)
+}
+else {clearInterval(intervalId)}})
   function update() {
       counter += 1
       time.textContent = counter
